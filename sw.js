2b8ea78c1cf62b59c588430f7a1cdfae224224ce
FFRfree <free.sun@foxmail.com>
2023-01-17 15:46:19 +0800

    eidt cache items

--- sw.js
+++ sw.js
@@ -63,35 +63,6 @@ self.addEventListener('install', (event) => {
     addResourcesToCache([
       '/',
       'index.html',
-      'otherData/MBMv5_13lobe+nuclei.nii.gz',
-      'otherData/MBMv5_13lobe.nii.gz',
-      'otherData/MBMv5_17lobe+nuclei.nii.gz',
-      'otherData/MBMv5_17lobe.nii.gz',
-      'otherData/MBMv5_Template.nii.gz',
-      'otherData/Template_sym_MTR_80um_CERB_small.nii.gz',
-      'otherData/atlas_Marmoset_CERB_13lobe_change_gmmasked.nii.gz',
-      'otherData/atlas_Marmoset_CERB_13lobe_group.shape.gii',
-      'otherData/atlas_Marmoset_CERB_17lobe_change_gmmasked.nii.gz',
-      'otherData/atlas_Marmoset_CERB_17lobe_group.shape.gii',
-      'otherData/sub_all_5k_8k_gradient1_group.func.gii',
-      'otherData/sub_all_gradient1_group.func.gii',
-      'otherData/surfFS.CERB.pial_shifti.surf(1).gii',
-      'gradients/shortMBMv5_Cerebello-cerebellar_gradient1.func.gii',
-      'gradients/shortMBMv5_Cerebello-cerebellar_gradient2.func.gii',
-      'gradients/shortMBMv5_Cerebello-cerebellar_gradient3.func.gii',
-      'gradients/shortMBMv5_Intra-cerebellar_gradient1.func.gii',
-      'gradients/shortMBMv5_Intra-cerebellar_gradient2.func.gii',
-      'gradients/shortMBMv5_Intra-cerebellar_gradient3.func.gii',
-      'gradients/short_MBMv5_Cerebello-cerebellar_gradient1.nii.gz',
-      'gradients/short_MBMv5_Cerebello-cerebellar_gradient2.nii.gz',
-      'gradients/short_MBMv5_Cerebello-cerebellar_gradient3.nii.gz',
-      'gradients/short_MBMv5_Intra-cerebellar_gradient1.nii.gz',
-      'gradients/short_MBMv5_Intra-cerebellar_gradient2.nii.gz',
-      'gradients/short_MBMv5_Intra-cerebellar_gradient3.nii.gz',
-      'gradients/short_MBMv5_nuclei_neocortex_gradient1.nii.gz',
-      'gradients/short_MBMv5_nuclei_neocortex_gradient2.nii.gz',
-      'gradients/short_MBMv5_nuclei_neocortex_gradient3.nii.gz'
-
     ])
   );
 });
